d749a26b7a4a9e0e53410c5ea832d65a9b3c5746
raxraj816 <raxraj816@gmail.com>
2021-06-16 05:00:34 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
+import React, { useEffect, useState } from "react";
 import { useForm } from "./useForm";
+// import { Hello } from "./Hello";
+import { useFetch } from "./useFetch";
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: "",
+  });
+  // const [showHello, setShowHello] = useState(true);
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count") ?? 0)
+  );
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count ?? 0));
+  }, [count]);
+
+  useEffect(() => {
+    console.log("render");
+  }, [values.firstName]);
+
   return (
-    <>
-      <input name="email" value={values.email} onChange={handleChange} />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-    </>
+    <div>
+      <div>{!data && loading ? "loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>increment</button>
+      <>
+        {/* <button onClick={() => setShowHello(!showHello)}>toggle</button>
+        {showHello && <Hello />} */}
+        <input name="email" value={values.email} onChange={handleChange} />
+        <input
+          name="firstName"
+          placeholder="first name"
+          value={values.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </>
+    </div>
   );
 };
 
---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+export const Hello = () => {
+  useEffect(() => {
+    console.log("render");
+
+    return () => {
+      console.log("unmount");
+    };
+  }, []);
+
+  return <div>hello</div>;
+};
