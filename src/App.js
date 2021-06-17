ae067b52ccf861b8b3b98fa3a1641b22d6af82b2
raxraj816 <raxraj816@gmail.com>
2021-06-18 05:16:00 +0530

    useContext

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import { useForm } from "./useForm";
+import React, { useState, useMemo } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Index } from "./pages";
+import { About } from "./pages/about";
+import { UserContext } from "./UserContext";
 
-const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
+function AppRouter() {
+  const [user, setUser] = useState(null);
+
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
-    <>
-      <input name="email" value={values.email} onChange={handleChange} />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <UserContext.Provider value={value}>
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+        </UserContext.Provider>
+      </div>
+    </Router>
   );
-};
+}
 
-export default App;
+export default AppRouter;
